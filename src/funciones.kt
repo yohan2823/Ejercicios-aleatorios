74ba02734646cb92a729ef0a60056ad942a5faf9
yohan2823 <yohanlopez345@gmail.com>
2024-08-20 11:11:01 -0500

    funciones en kotlin

--- src/funciones.kt
+++ src/funciones.kt
@@ -1,17 +1,55 @@
+//Yohan Yamith Lopez Yomayuza Id 815392
+import jdk.internal.jimage.decompressor.CompressIndexes.readInt
 fun main (){
-    suma()
-    resta()
-    raiz()
-    println("la raiz es:${raiz()}")
-    println(cuadrado())
-    println(saludo())
-    division(n1,n2)
+    //suma()
+    //resta()
+    //raiz()
+    //println("la raiz es:${raiz()}")
+    //println(cuadrado())
+    //println(saludo())
+    //division(n1,n2)
+    while (true) {
+        println("Selecciona una opción:")
+        println("1. Sumar")
+        println("2. Restar")
+        println("3. Raíz")
+        println("4. Cuadrado")
+        println("5. Saludo")
+        println("6. Dividir")
+        println("7. Multiplicar")
+        println("8. Salir")
 
+        print("Opción: ")
+        val option = readlnOrNull()?.toInt()
 
+        if (option == 1) {
+            suma()
+        } else if (option == 2) {
+            resta()
+        } else if (option == 3) {
+            println("La raíz es: ${raiz()}")
+        } else if (option == 4) {
+            println("El cuadrado es: ${cuadrado()}")
+        } else if (option == 5) {
+            println(saludo())
+        } else if (option == 6) {
+            division(n1, n2)
+        } else if (option == 7) {
+            println(multi(n1.toDouble(), n2.toDouble()))
+        } else if (option == 8) {
+            println("Saliendo del programa...")
+            break
+        } else {
+            println("Opción no válida, por favor selecciona una opción del 1 al 8.")
+        }
+        println()
+    }   
 }
-var n1=4
-var n2=8
 
+
+
+var n1=4
+var n2=82
 fun suma(){
     var suma=n1+n2
     println("la suma es:$suma")
