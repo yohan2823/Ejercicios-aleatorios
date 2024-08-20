d1493ebbe93ffe2df5ffcf92d79e32e480d6eced
yohan2823 <yohanlopez345@gmail.com>
2024-08-20 10:08:16 -0500

    juego cara sello

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+
+    val listaMixta: List<Any> = listOf(1, "dos", 3.0, true)
+
+    println("Lista completa: $listaMixta")
+
+    for (elemento in listaMixta) {
+        println(elemento)
+    }
 }
